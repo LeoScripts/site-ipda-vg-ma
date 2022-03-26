ac53ab680932bff19e423416d4b7aa414708bb80
Leandro Cavalcante <leandroguitarjesus89@gmail.com>
2022-03-26 10:29:18 -0300

    slid

---
+++ vue/src/components/Nav.vue
@@ -0,0 +1,17 @@
+<template>
+<header>
+<img src="logo" alt="">
+<strong>....</strong>
+</header>
+</template>
+<script>
+import logo from '../assets/Logotipo-IPDA.jpg'
+
+  export default {
+    data () {
+      return {
+        logo
+      }
+    },
+  }
+</script>
